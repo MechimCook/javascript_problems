7782403fc0c6e8cad8c5b2be68445064abd50546
Mechim <mechim.cook20@gmail.com>
2020-06-30 14:20:54 -0400

    Space age V2

--- space-age/space-age.js
+++ space-age/space-age.js
@@ -2,10 +2,5 @@
 // This is only a SKELETON file for the 'Space Age' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-const earth_year = 31557600;
-const years = {'mercury': 0.2408467,'venus': 0.61519726, 'earth': 1,'mars': 1.8808158, 'jupiter': 11.862615, 'saturn': 29.447498,  'uranus': 84.016846, 'neptune': 164.79132};
-
-export const age = (planet, base_age) =>  {
-  const planet_years = Math.round(base_age / earth_year / years[planet] * 100)/100
-  return planet_years;
-};
+const years = {'mercury': 7600543.81992,'venus': 19414149.052176, 'earth': 31557600,'mars': 59354032.690079994, 'jupiter': 374355659.124, 'saturn': 929292362.8848,  'uranus': 2651370019.3296, 'neptune': 5200418560.032001};
+export const age = (planet, base_age) =>  Math.round(base_age / years[planet] * 100)/100;
